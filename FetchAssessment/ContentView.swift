2b06801c6dc904fa5c422fe80374492755d5d510
Ashika kalmady <113388897+AshikaKalmady@users.noreply.github.com>
2024-02-27 21:09:19 -0500

    Initial Commit

---
+++ FetchAssessment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchAssessment
+//
+//  Created by ashika kalmady on 2/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchAssessment/FetchAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchAssessmentApp.swift
+//  FetchAssessment
+//
+//  Created by ashika kalmady on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
